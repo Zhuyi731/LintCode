b07d92def384cb416c76c8313259ab95af0702c0
zhuyi <zhuyi@ideamake.cn>
2019-06-14 14:22:43 +0800

    24点问题解决

--- Interview_problems/24点问题_simple.js
+++ Interview_problems/24点问题_simple.js
@@ -9,7 +9,6 @@
 // aaa++a+
 // aa+aa++
 // aa+a+a+
-const fs = require('fs')
 let startTime = Date.now()
 get24Possibility()
 let endTime = Date.now()
@@ -22,9 +21,10 @@ function get24Possibility () {
 
     let temp
     for (i1 = 1; i1 < 11; i1++) {
+        // console.log(`已计算${i1 - 1}000个`)
         for (i2 = 1; i2 < 11; i2++) {
             for (i3 = 1; i3 < 11; i3++) {
-                console.log(`已计算${i1 - 1}${i2 - 1}${i3 - 1}0个`)
+                // console.log(`已计算${i1 - 1}${i2 - 1}${i3 - 1}0个`)
                 for (i4 = 1; i4 < 11; i4++) {
                     min = [i1, i2, i3, i4].map(el => {
                         return el == 10 ? 0 : el
@@ -58,22 +58,17 @@ function get24Possibility () {
 
 
     let possibles = [], impossibles = []
-    let singleSolution = []
-    let finalExpressions = []
     for (let prop in solutions) {
         if (solutions[prop].ct === 0) {
             impossibles.push(prop)
         } else {
-            //去重并计算单一表达式的情况  
             possibles.push(prop)
-
-            finalExpressions = filterDuplicate(solutions[prop].expression)
-            if (finalExpressions.length === 1) {
-                singleSolution.push(finalExpressions)
-            }
         }
     }
-    console.log(possibles.length, impossibles.length, singleSolution.length)
+    console.log('所有组合数' + (possibles.length + impossibles.length))
+    console.log('可计算的24点个数' + possibles.length)
+    console.log('不可计算的24点个数' + impossibles.length)
+    console.log('概率' + (possibles.length * 100 / (possibles.length + impossibles.length)).toFixed(2) + '%')
 }
 
 function filterDuplicate (expressions) {
@@ -92,12 +87,8 @@ function filterDuplicate (expressions) {
 
 function GetReversePolishNotation (expression) {
     //根据逆波兰表达式来计算值
-    // let result
     let numberStack = []
     let num1, num2, curNum, temp
-    let operatorProcess = ""
-    let isFirstOperator = true
-    let currentIndex = -1
     expression.forEach(el => {
         if (typeof el !== 'number') {//表达式
             num1 = numberStack.pop()
@@ -116,48 +107,13 @@ function GetReversePolishNotation (expression) {
                     curNum = num1 / num2
                 } break;
             }
-            if (isFirstOperator) {
-                if (el == '+' || el == "*") {
-                    if (num1 > num2) {
-                        num1 = num1 ^ num2
-                        num2 = num1 ^ num2
-                        num1 = num1 ^ num2
-                    }
-                }
-                operatorProcess = [num1, el, num2]
-            } else {
-                if (el === '+' || el === "*") {
-                    operatorProcess.push(el)
-                    operatorProcess.push(currentIndex === 1 ? num1 : num2)
-                } else {
-                    if (currentIndex === 1) {
-                        operatorProcess.shift(el)
-                        operatorProcess.shift(num1)
-                    } else {
-                        operatorProcess.push(el)
-                        operatorProcess.push(num2)
-                    }
-                }
-            }
-            isFirstOperator && (isFirstOperator = false)
             numberStack.push(curNum)
-            currentIndex = 0
         } else {
             numberStack.push(el)
-            currentIndex = 1
         }
     })
-    //获取到operatorProcess之后再进行一遍排序
-    for (let i = 0; i < operatorProcess.length; i++) {
-        if (operatorProcess[i] == '+' || operatorProcess[i] === "*") {
-            if (operatorProcess[i - 1] > operatorProcess[i + 1]) {
-                temp = operatorProcess[i - 1]
-                operatorProcess[i - 1] = operatorProcess[i + 1]
-                operatorProcess[i + 1] = temp
-            }
-        }
-    }
-    return [numberStack[0], operatorProcess.join('')]
+
+    return numberStack[0]
 }
 
 function isPossible (i1, i2, i3, i4) {
@@ -181,8 +137,8 @@ function isPossible (i1, i2, i3, i4) {
                 allSituation.forEach(el => {
                     // 计算数据stack和operator stack
                     let calculate = GetReversePolishNotation(el)
-                    if (Math.abs(calculate[0] - 24) < 0.0002) {//设置一个阈值  避免浮点数计算不精确的问题
-                        ret.push(calculate[1])
+                    if (Math.abs(calculate - 24) < 0.0002) {
+                        ret.push(el)
                     }
                 })
             }
--- Interview_problems/filter.js
+++ Interview_problems/filter.js
@@ -1,54 +1,19 @@
 const fs = require("fs")
 
-possible = fs.readFileSync('./interview_problems/possible.txt', 'utf-8')
-impossible = fs.readFileSync('./interview_problems/impossible.txt', 'utf-8')
+let pos1 = fs.readFileSync('./1.txt', 'utf-8')
+let pos2 = fs.readFileSync('./3.txt', 'utf-8')
 
-possible = possible.split("\r\n").filter(el => /组合为/.test(el))
-impossible = impossible.split("\r\n")
 
-possible = possible.map(el => {
-    match = /组合为:(.*)/.exec(el)
-    tt = match[1].split(" ").map(el => ~~el)
-    tt = tt.sort((e1,e2)=>e1>e2)
-    return match[1].split(" ").map(el => ~~el).sort((e1,e2)=>e1>e2).map(el => {
-        if (el == "10") {
-            return 0
-        } else {
-            return el
-        }
-    }).join("")
-})
+pos1 = pos1.split('\r\n').map(el => el.split(',').sort((a, b) => ~~a > ~~b).join(','))
+pos2 = pos2.split('\n').filter(el => /算不起/.test(el)).map(el => el.split(' :')[0].split(' ').sort((a, b) => ~~a > ~~b).join(','))
 
-impossible = impossible.map(el => {
-    return el.split(" ").map(el => ~~el).sort().map(el => {
-        if (el == "10") {
-            return 0
-        } else {
-            return el
-        }
-    }).join("")
-})
 
-//然后去重
-var len = possible.length
-for (var i = 0; i < len - 1; i++) {
-    for (var j = i + 1; j < len; j++) {
-        if (possible[i] == possible[j]) {
-            possible.splice(j, 1)
-            len--
-        }
+
+let ret = []
+pos1.forEach(el => {
+    if (!pos2.includes(el)) {
+        ret.push(el)
     }
-}
+});
 
-len = impossible.length
-for (var i = 0; i < len - 1; i++) {
-    for (var j = i + 1; j < len; j++) {
-        if (impossible[i] == impossible[j]) {
-            impossible.splice(j, 1)
-            len--
-        }
-    }
-}
-console.log(possible.length)
-console.log(impossible.length)
-console.log(impossible.length / possible.length)
+console.log(ret)
