103f40d08f0a83b0fce48b71d07ac787722daac1
Zhuyi731 <604032172@qq.com>
2020-01-17 11:20:02 +0800

    34题: 二分查找

--- LeetCode/34. Find First and Last Position of Element in Sorted Array/index34.js
+++ LeetCode/34. Find First and Last Position of Element in Sorted Array/index34.js
@@ -3,37 +3,100 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
+var searchRange = function (nums, target) {
+    if (nums.length === 0) return [-1, -1]
     // 首先 二分法查找出最左边的解  
-    let resultL= -1,resultR = -1 
-    let l=0,r=nums.length,mid = Math.floor((l+r)/2) 
-    while(1){
-       
-        if(target > nums[mid]){
-            l = mid 
-            mid =  Math.floor((l+r)/2)
-        }else if(target > nums[mid]){
-            r=mid 
-            mid =  Math.floor((l+r)/2)
-        }else{
-            if(mid=0 || nums[mid]!=target){
-                //说明找到最左边的了  
-                resultL = mid  
-                resultR = mid 
+    let resultL = -1, resultR = -1
+    let l = 0,
+        r = nums.length - 1,
+        mid = Math.floor((l + r) / 2)
+    while (1) {
+        if (l >= r - 1) {
+            if (target === nums[l]) {
+                mid = l
+                resultL = l
+                resultR = l
                 break;
-            }else{
-                r=mid 
-                mid =  Math.floor((l+r)/2)
+            }
+            if (target === nums[r]) {
+                mid = r
+                resultL = r
+                resultR = r
+                break;
+            }
+            break
+        }
+        if (target < nums[mid]) {
+            r = mid
+            mid = Math.floor((l + r) / 2)
+        } else if (target > nums[mid]) {
+            l = mid
+            mid = Math.floor((l + r) / 2)
+        } else {
+            if (mid === 0 || nums[mid - 1] != target) {
+                //说明找到最左边的了  
+                resultL = mid
+                resultR = mid
+                break;
+            } else {
+                r = mid
+                mid = Math.floor((l + r) / 2)
             }
         }
     }
-
+    if (resultL === -1) return [-1, -1]
+    l = mid
+    r = nums.length - 1
+    mid = Math.floor((l + r) / 2)
+    while (1) {
+        if (l >= r - 1) {
+            if (target === nums[r]) {
+                resultR = r
+                break;
+            }
+            if (target === nums[l]) {
+                resultR = l
+                break;
+            }
+            break
+        }
+        if (target < nums[mid]) {
+            r = mid
+            mid = Math.floor((l + r) / 2)
+        } else if (target > nums[mid]) {
+            l = mid
+            mid = Math.floor((l + r) / 2)
+        } else {
+            if (mid === nums.length - 1 || nums[mid + 1] != target) {
+                //说明找到最右边的了  
+                // resultL = mid
+                resultR = mid
+                break;
+            } else {
+                l = mid
+                mid = Math.floor((l + r) / 2)
+            }
+        }
+    }
     // 然后找最右边的   
-    return [l,r]
+    return [resultL, resultR]
 };
 
-console.log(searchRange([5,7,7,8,8,10],8))
-console.log(searchRange([5,7,7,8,8,10],6))
+
+console.log(searchRange([1, 2, 3], 1))
+console.log(searchRange([1, 3], 1))
+console.log(searchRange([1, 3], 3))
+console.log(searchRange([3, 3], 3))
+console.log(searchRange([3], 3))
+console.log(searchRange([1, 1], 3))
+console.log(searchRange([1], 3))
+console.log(searchRange([1, 8, 8, 8, 8, 8, 8, 8, 8, 8], 3))
+console.log(searchRange([8, 8, 8, 8, 8, 8, 8, 8, 8, 10], 8))
+console.log(searchRange([1, 8, 8, 8, 8, 8, 8, 8, 8, 8], 8))
+console.log(searchRange([8, 8, 8, 8, 8, 8, 8, 8, 8], 8))
+console.log(searchRange([5, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 10], 8))
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8))
+console.log(searchRange([5, 7, 7, 8, 8, 10], 6))
 
 // Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 
