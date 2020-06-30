6168543c1fba70c3a116d9f059920b8834a67bd8
zoeizhu <zoeizhu@tencent.com>
2020-06-30 14:48:03 +0800

    51题:困难 8皇后问题

---
+++ LeetCode/51.N-Queens/index51.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function (n) {
+  let result = [];
+
+  dfs(0, n, [], []);
+
+  function dfs(curDepth, depth, usedPos, curMatrix) {
+    if (curDepth === depth) {
+      return result.push(curMatrix);
+    }
+    for (let i = 0; i < depth; i++) {
+      let usedPos_bak = usedPos.map((el) => el);
+      let curMatrix_bak = curMatrix.map((el) => el);
+      if (usedPos_bak[i]) {
+        continue;
+      }
+      if (checkCanAttack(curDepth, depth, i, curMatrix_bak)) {
+        continue;
+      }
+      // 如果可行，则递归
+      usedPos_bak[i] = 1;
+      let tmp = Array(depth).fill(".");
+      tmp[i] = "Q";
+      curMatrix_bak.push(tmp);
+      dfs(curDepth + 1, depth, usedPos_bak, curMatrix_bak);
+    }
+  }
+  function checkCanAttack(curDepth, depth, i, curMatrix) {
+    for (let j = 0; j < curDepth; j++) {
+      let minus = curDepth - j;
+      if (i - minus >= 0 && curMatrix[j][i - minus] === "Q") {
+        return true;
+      }
+      if (i + minus < depth && curMatrix[j][i + minus] === "Q") {
+        return true;
+      }
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+solveNQueens(4);
+// [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
