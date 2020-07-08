25ad23292be247348f0e7bd9c17aef04d704dee9
zoeizhu <zoeizhu@tencent.com>
2020-07-08 11:45:20 +0800

    83题: 中等 移除链表中的重复元素

--- LeetCode/83. Remove Duplicates from Sorted List/index83.js
+++ LeetCode/83. Remove Duplicates from Sorted List/index83.js
@@ -9,11 +9,34 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
-
+var deleteDuplicates = function (head) {
+  if (!head) return head;
+  let prev = {
+    next: head,
+  };
+  let cur = head;
+  let next = head.next;
+  let prev_bak = prev;
+  while (cur && next) {
+    if (next.val === cur.val) {
+      cur.next = next.next;
+      next = cur.next;
+    } else {
+      cur = cur.next;
+      cur && (next = cur.next);
+    }
+  }
+  return prev_bak.next;
 };
 
+const { ListNodeGeneratorByArr } = require("../../js_utils/index");
 
+deleteDuplicates(ListNodeGeneratorByArr([1, 1]));
+deleteDuplicates(ListNodeGeneratorByArr([1, 2, 3, 3, 4, 4, 5]));
+deleteDuplicates(ListNodeGeneratorByArr([1, 1, 1, 2, 3]));
+deleteDuplicates(ListNodeGeneratorByArr([3, 2, 1, 1, 1]));
+deleteDuplicates(ListNodeGeneratorByArr([1, 1, 1]));
+deleteDuplicates(ListNodeGeneratorByArr([]));
 
 // Given a sorted linked list, delete all duplicates such that each element appear only once.
 
