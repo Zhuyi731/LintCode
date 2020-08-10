b04bb16d1fd1a09b4bd759d9ae443068d1485a9b
zoeizhu <zoeizhu@tencent.com>
2020-08-10 15:45:58 +0800

    阿里面试题: 简单  1.数组排序  2.throttle实现 3.约瑟夫环问题

---
+++ Interview_problems/aliInterviewProblem.js
@@ -0,0 +1,31 @@
+/*
+1. Array merge and reorder, any ES version can be used
+
+Input: two arrays which contain integer
+Output: merged array and sort from small to large
+*/
+
+
+
+
+
+/*
+2. Implement throttle function, take the example of judging whether the page scrolls at the bottom
+*/
+
+
+
+
+
+
+
+
+
+/*
+3. There are n people in a circle, numbered from 1 to n in order. 
+The first person starts counting, the person reporting m (m<n) exits the circle, 
+the next person starts counting from 1, and the person reporting m exits the circle. 
+So go on until the last person is left. 
+Enter the integers n and m, and output the number of the person exiting the circle in the order of exit. 
+Try to write the corresponding program.
+*/
