c20e050716d04a9144864026c34936f5b1a393e6
zoeizhu <zoeizhu@tencent.com>
2020-08-12 17:17:09 +0800

    add: 添加go 无向图生成工具函数

--- go_utils/node/createNode.go
+++ go_utils/node/createNode.go
@@ -1,15 +1,17 @@
 package node
 
 import (
-	"strings"
 	"strconv"
+	"strings"
 )
 
+// ListNode 链表结构
 type ListNode struct {
-	Val int
-	Next *ListNode
+	Val  int       //当前节点的值
+	Next *ListNode // 下一节点指针
 }
 
+// CreateListNode 通过string 创建链表结构
 func CreateListNode(nodeString string) *ListNode {
 	nodeList := ListNode{}
 	node := strings.Split(nodeString, "->")
@@ -24,4 +26,32 @@ func CreateListNode(nodeString string) *ListNode {
 	}
 
 	return &nodeList
+}
+
+// Node 无向图结构
+type Node struct {
+	Val       int     //当前节点值
+	Neighbors []*Node //相邻节点
+}
+
+// CreateNodeBySlice 通过数组创建无向图
+func CreateNodeBySlice(arr [][]int) *Node {
+	nodeList := make([]Node, len(arr))
+
+	for i := 0; i < len(arr); i++ {
+		nodeList[i] = Node{
+			Val: i + 1,
+		}
+		tempNodeList := make([]*Node, len(arr[i]))
+		for j := 0; j < len(arr[i]); j++ {
+			currentIndex := arr[i][j] - 1
+			if nodeList[currentIndex].Val != currentIndex+1 { // 说明没有创建过
+				nodeList[currentIndex].Val = currentIndex + 1
+			}
+
+			tempNodeList[j] = &nodeList[currentIndex]
+		}
+		nodeList[i].Neighbors = tempNodeList
+	}
+	return &nodeList[0]
 }
