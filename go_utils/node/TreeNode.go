b3aba7c2b7bc7ec4079b63def436992e33149dfd
zoeizhu <zoeizhu@tencent.com>
2021-05-11 14:39:56 +0800

    feat: 添加utils

--- go_utils/node/TreeNode.go
+++ go_utils/node/TreeNode.go
@@ -1,4 +1,4 @@
-package TreeNode
+package node
 
 import (
 	"fmt"
@@ -10,6 +10,43 @@ type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+const NULL = 0X7FFFFFFF
+const null = 0x7FFFFFFF
+
+func Ints2TreeNode(ints []int) *TreeNode {
+	n := len(ints)
+	if n == 0 {
+		return nil
+	}
+
+	root := &TreeNode{
+		Val: ints[0],
+	}
+
+	queue := make([]*TreeNode, 1, n*2)
+	queue[0] = root
+
+	i := 1
+	for i < n {
+		node := queue[0]
+		queue = queue[1:]
+
+		if i < n && ints[i] != NULL {
+			node.Left = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Left)
+		}
+		i++
+
+		if i < n && ints[i] != NULL {
+			node.Right = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Right)
+		}
+		i++
+	}
+
+	return root
 }
 
 // 生成树结构
